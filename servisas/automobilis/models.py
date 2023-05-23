c7250cfad3ed61c2cfc89699fc1194212d05308a
Edgaras <trintuxs@gmail.com>
2023-05-23 20:58:11 +0300

    update

---
+++ servisas/automobilis/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Auto(models.model):
+    Marke = models.CharField(max_length= 80)
+    MOdelis = models.CharField(max_length= 80)
+    Valstybinis = models.CharField(max_length= 80)
+    VIN_kodas = models.CharField(max_length= 80)
+    Klientas = models.CharField(max_length= 80)
