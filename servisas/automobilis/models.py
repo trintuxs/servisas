5094e1f9416e7769e6885ea18d74aca1e6d53445
Edgaras <trintuxs@gmail.com>
2023-05-23 21:17:50 +0300

    update

--- servisas/automobilis/models.py
+++ servisas/automobilis/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Auto(models.model):
+class Auto(models.Model):
     Marke = models.CharField(max_length= 80)
     MOdelis = models.CharField(max_length= 80)
     Valstybinis = models.CharField(max_length= 80)
